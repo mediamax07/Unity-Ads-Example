49bf435372395f4188868b2ea492b13ddebd07f5
Yassine Boumaiza <31522077+mediamax07@users.noreply.github.com>
2020-05-12 21:13:42 +0100

    Add files via upload

---
+++ ShowAdScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class ShowAdScript : MonoBehaviour {
+	public void PlayAd () {
+		if (Advertisement.IsReady ()) {
+			Advertisement.Show ("rewardedVideo", new ShowOptions { resultCallback = AdResult });
+		}
+	}
+
+	void AdResult (ShowResult showResult) {
+		switch (showResult) {
+		case ShowResult.Failed:
+			Debug.Log ("Ad failed");
+			break;
+		case ShowResult.Skipped:
+			Debug.Log ("Ad skipped");
+			break;
+		case ShowResult.Finished:
+			Debug.Log ("Ad successfully finished (Make a reward)");
+			break;
+		}
+	}
+}
